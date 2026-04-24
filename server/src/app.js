d72f34153d22fdf3a9f7a6ebdf7ba998b5de9848
Sriyansh <yadavsriyansh400@gmail.com>
2026-04-24 20:22:30 +0000

    fix cors 4

--- server/src/app.js
+++ server/src/app.js
@@ -5,10 +5,19 @@ import noteRoutes from "./routes/noteRoutes.js";
 const app = express();
 
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
-
 app.use("/api/notes", noteRoutes);
 
 app.get("/", (req, res) => {
